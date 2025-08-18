f70e1b002cb6379f4d7a91189aea639b98d9ba14
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-08-19 02:36:05 +0530

    Initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./About.css"
+function About() {
+  return (
+    <div id="about">
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Projects.css"
+
+function Projects() {
+  return (
+    <div id="projects"></div>
+  )
+}
+
+export default Projects
