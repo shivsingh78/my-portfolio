b68db7817caf9b43760b2bec23d98c7ef4a2fee9
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-08-19 18:55:06 +0530

    Add new dependencies and enhance component styles
    - Updated package.json and package-lock.json to include react-simple-typewriter, react-typing-effect, and typewritter.
    - Modified About.css and About.jsx to improve layout and add new elements.
    - Enhanced Home.css and Home.jsx for better styling and added typewriter effect.
    - Updated Nav.css for improved navigation styling.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,17 @@ import "./About.css"
 function About() {
   return (
     <div id="about">
+      <div className="leftabout">
+        <div className="circle-line">
+          <div className="circle"></div>
+          <div className="line"></div>
+          <div className="circle"></div>
+          <div className="line"></div>
+          <div className="circle"></div>
+          
+        </div>
+      </div>
+      <div className="rightabout"></div>
     </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import "./Home.css"
 import shiv from "../../assets/shiv.png"
-import man from "../../assets/man.png"
 
+// import man from "../../assets/man.png"
+import { Typewriter } from 'react-simple-typewriter'
 function Home() {
   return (
     <div id="home">
      <div className="lefthome">
       
+      <div className="homedetails">
+        <div className="line1" style={{fontWeight: "bold"}}>I'M</div>
+        <div className="line2">SHIV SINGH BAGHEL</div>
+        <div className="line3"> 
+          <span style={{ color: "", fontWeight: "bold" }}>
+          <Typewriter
+            words={["FULL STACK WEB DEVLOPER ", "SOFTWARE DEVELOPER","PYTHON DEVLOPER", "PROBLEM SOLVER 🚀"]}
+            loop={0} // 0 = infinite
+            cursor
+            cursorStyle="|"
+            typeSpeed={50}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            
+          />
+        </span>
+        </div>
+        <button>HIRE ME</button>
+      </div>
+     
 
      </div>
      <div className="righthome">
