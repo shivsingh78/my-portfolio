f2453b29144d94137dbc56a7fce78018f2e71b93
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-08-20 02:41:06 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import "./Contact.css"
+import contactImg from '../../assets/contact.png'
 function Contact() {
   return (
     <div id="contact">
+      <div className="leftcontact">
+        <img src={contactImg} alt="" />
+      </div>
+      <div className="rightcontact">
+        <form action="https://formspree.io/f/xzzvlrek" method='POST'>
+          <input name='Username' type="text" placeholder='Name' />
+          <input name='Email' type="email" placeholder='Email' />
+          <textarea name="message" id="textarea" placeholder='message me'>
+
+          </textarea>
+          <input type="submit" id='btn' value="Submit"/>
+        </form>
+      </div>
 
     </div>
   )
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import "./Projects.css"
 import Card from '../Card/Card'
+import va from '../../assets/va.png'
+import lms from '../../assets/lms.png'
+import ec from '../../assets/ec.png'
+import rt from '../../assets/rt.png'
+import yt from '../../assets/yt.png'
 
 function Projects() {
   return (
     <div id="projects">
-      <h1>1+ YEARS EXPERIENCED IN PROJECTS</h1>
+      <h1 id='para'>1+ YEARS EXPERIENCED IN PROJECTS</h1>
       <div className="slider">
         <Card title="VIRTUAL ASSISTANT" image={va}/>
-        <Card title="" image={va}/>
-        <Card title="" image={va}/>
-        <Card title="" image={va}/>
-        <Card title="" image={va}/>
+        <Card title="LEARNING MANAGEMENT SYSTEM" image={lms}/>
+        <Card title="E-COMMERCE STORE" image={ec}/>
+        <Card title="REAL TIME COMMUNICATION APP" image={rt}/>
+        <Card title="BACKEND FOR YOUTUBE" image={yt}/>
+        
       </div>
     </div>
   )
