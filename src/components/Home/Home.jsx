cef8ac5188eaa422913069f4b860748ae73ecb7b
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-08-19 15:22:19 +0530

    assets added from locat storage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./Home.css"
+import shiv from "../../assets/shiv.png"
+import man from "../../assets/man.png"
 
 function Home() {
   return (
     <div id="home">
+     <div className="lefthome">
+      
+
+     </div>
+     <div className="righthome">
+      <img src={shiv} alt=""  />
+     </div>
+
 
     </div>
   )
